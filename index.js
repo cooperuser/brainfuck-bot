ed3a13a224c20189e1e612ef544fdd017fcaa4b2
cooper-anderson <cooper@cooperstuff.ninja>
2016-12-24 02:06:39 -0800

    Connection with bot client

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const Discord = require("discord.js");
 settings = require("./settings.json");
+bot = new Discord.Client();
+
+bot.login(settings.token);
+
+bot.on("message", function(message) {
+	console.log(message.content);
+});
